18634f67650fa99ec875e221d09bf703b463da6d
khaingthazindev <khaingthazin.dev@gmail.com>
2025-05-02 16:46:22 +0630

    🚧 [WIP] admin panel layout is still working

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,29 +1,114 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>@yield('title')</title>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+	<title>@yield('title')</title>
 
-        <!-- Scripts -->
-        @vite(['resources/scss/app.scss', 'resources/js/app.js'])
-    </head>
-    <body class="tw-font-sans tw-antialiased">
-        <div class="tw-min-h-screen tw-bg-gray-100">
-            @include('layouts.navigation')
+	<!-- Fonts -->
+	<link rel="preconnect" href="https://fonts.bunny.net">
+	<link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-            <!-- Page Content -->
-            <main>
-                @yield('content')
-            </main>
-        </div>
-    </body>
+	<!-- Google Font: Source Sans Pro -->
+	<link rel="stylesheet"
+		href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 
-    @stack('scripts')
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="{{asset('plugins/fontawesome-free/css/all.min.css')}}">
+
+	<!-- Ionicons -->
+	<link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+
+	<!-- Theme style -->
+	<link rel="stylesheet" href="{{asset('css/adminlte.min.css')}}">
+
+	<!-- overlayScrollbars -->
+	<link rel="stylesheet" href="{{asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
+
+	<!-- Daterange picker -->
+	<link rel="stylesheet" href="{{asset('plugins/daterangepicker/daterangepicker.css')}}">
+
+	<!-- summernote -->
+	<link rel="stylesheet" href="{{asset('plugins/summernote/summernote-bs4.min.css')}}">
+
+	<!-- Scripts -->
+	@vite(['resources/scss/app.scss', 'resources/js/app.js'])
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+	<div class="wrapper">
+
+		@include('layouts.preloader')
+
+		@include('layouts.navigation')
+
+		@include('layouts.sidebar')
+
+		<!-- Content Wrapper. Contains page content -->
+		<div class="content-wrapper">
+			<!-- Content Header (Page header) -->
+			<div class="content-header">
+				<div class="container-fluid">
+					<div class="row mb-2">
+						<div class="col-sm-6">
+							<h1 class="m-0">Dashboard</h1>
+						</div><!-- /.col -->
+						<div class="col-sm-6">
+							<ol class="breadcrumb float-sm-right">
+								<li class="breadcrumb-item"><a href="#">Home</a></li>
+								<li class="breadcrumb-item active">Dashboard v1</li>
+							</ol>
+						</div><!-- /.col -->
+					</div><!-- /.row -->
+				</div><!-- /.container-fluid -->
+			</div>
+			<!-- /.content-header -->
+
+			<!-- Main content -->
+			<section class="content">
+				<div class="container-fluid">
+					@yield('content')
+				</div><!-- /.container-fluid -->
+			</section>
+			<!-- /.content -->
+		</div>
+		<!-- /.content-wrapper -->
+		<footer class="main-footer">
+			<strong>Copyright &copy; 2014-2021 <a href="https://adminlte.io">AdminLTE.io</a>.</strong>
+			All rights reserved.
+			<div class="float-right d-none d-sm-inline-block">
+				<b>Version</b> 3.2.0-rc
+			</div>
+		</footer>
+
+		<!-- Control Sidebar -->
+		<aside class="control-sidebar control-sidebar-dark">
+			<!-- Control sidebar content goes here -->
+		</aside>
+		<!-- /.control-sidebar -->
+	</div>
+	<!-- ./wrapper -->
+
+	<!-- jQuery -->
+	<script src="{{asset('plugins/jquery/jquery.min.js')}}"></script>
+
+	<!-- Bootstrap 4 -->
+	<script src="{{asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+	<!-- daterangepicker -->
+	<script src="{{asset('plugins/moment/moment.min.js')}}"></script>
+	<script src="{{asset('plugins/daterangepicker/daterangepicker.js')}}"></script>
+
+	<!-- overlayScrollbars -->
+	<script src="{{asset('plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js')}}"></script>
+
+	<!-- AdminLTE App -->
+	<script src="{{asset('js/adminlte.js')}}"></script>
+</body>
+
+@stack('scripts')
+
 </html>
