78e9df19ce6d06a6b0559a3ce693037a063699d0
khaingthazindev <khaingthazin.dev@gmail.com>
2025-05-02 18:29:30 +0630

    ✅ [FINISH] admin panel has finished. config custom tailwind theme in tailwind.config.js

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,14 +2,21 @@
 
 @section('title', 'Dashboard')
 
+@section('header')
+   <div class="tw-flex tw-justify-between tw-items-center">
+      <div class="tw-flex tw-justify-between tw-items-center">
+        <i class="fas fa-th tw-p-3 tw-bg-white tw-rounded-lg tw-shadow tw-mr-1"></i>
+        <h5>Dashboard</h5>
+      </div>
+      <div></div>
+   </div>
+@endsection
+
 @section('content')
-<div class="tw-py-12">
-    <div class="tw-max-w-7xl tw-mx-auto sm:tw-px-6 lg:tw-px-8">
-        <div class="tw-bg-white tw-overflow-hidden tw-shadow-sm sm:tw-rounded-lg">
-            <div class="tw-p-6 tw-text-gray-900">
-                {{ __("You're logged in!") }}
-            </div>
-        </div>
-    </div>
-</div>
+   <x-card>
+      <div class="tw-flex tw-justify-center tw-items-center">
+        <img src="{{asset('image/dashboard.png')}}" alt="" class="tw-w-4/12">
+      </div>
+      <p class="text-center tw-text-gray-600">Welcome to {{config('app.name')}}</p>
+   </x-card>
 @endsection
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,22 +1,31 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
+import defaultTheme from "tailwindcss/defaultTheme";
+import forms from "@tailwindcss/forms";
 
 /** @type {import('tailwindcss').Config} */
 export default {
-    prefix: 'tw-',
+    prefix: "tw-",
     important: true,
     content: [
-        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        './storage/framework/views/*.php',
-        './resources/views/**/*.blade.php',
-        './app/Http/Controllers/**/*.php',
-        './app/Models/**/*.php',
+        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
+        "./storage/framework/views/*.php",
+        "./resources/views/**/*.blade.php",
+        "./app/Http/Controllers/**/*.php",
+        "./app/Models/**/*.php",
     ],
 
     theme: {
         extend: {
             fontFamily: {
-                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+                sans: ["Figtree", ...defaultTheme.fontFamily.sans],
+            },
+            backgroundImage: {
+                theme: "linear-gradient(90deg,#3CACB6 0%,#8CCEAD 90%)",
+            },
+            borderColor: {
+                theme: "#1CBC9B",
+            },
+            textColor: {
+                theme: "#1CBC9B",
             },
         },
     },
