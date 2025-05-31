10775f88bd23d4825f15396f48070107c260a0ac
khaingthazindev <khaingthazin.dev@gmail.com>
2025-05-31 16:51:58 +0630

    ✅ [FINISH] Api user register finish

--- app/Http/Controllers/Api/UserPortal/AuthController.php
+++ app/Http/Controllers/Api/UserPortal/AuthController.php
@@ -2,13 +2,51 @@
 
 namespace App\Http\Controllers\Api\UserPortal;
 
-use App\Http\Controllers\Controller;
+use App\Repositories\WalletRepository;
 use Illuminate\Http\Request;
+use App\Services\ResponseService;
+use Illuminate\Support\Facades\DB;
+use App\Http\Controllers\Controller;
+use App\Repositories\UserRepository;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
     public function register(Request $request)
     {
+        $request->validate([
+            'name' => ['required', 'string', 'max:100'],
+            'email' => ['required', 'email', 'unique:users,email'],
+            'password' => ['required', 'string', 'min:8', 'max:20'],
+        ]);
 
+        DB::beginTransaction();
+        try {
+            $user = (new UserRepository())->create([
+                'name' => $request->name,
+                'email' => $request->email,
+                'password' => Hash::make($request->password),
+            ]);
+
+            (new WalletRepository())->firstOrCreate(
+                ['user_id' => $user->id],
+                ['amount' => 0]
+            );
+
+            DB::commit();
+
+            $token = $user->createToken(config('app.name'))->plainTextToken;
+
+            return [
+                'data' => [
+                    'access_token' => $token
+                ],
+                'message' => 'Successfully registered',
+            ];
+
+        } catch (\Exception $e) {
+            DB::rollBack();
+            return ResponseService::fail($e->getMessage());
+        }
     }
 }
